c49ccbe495f1e7f686f88809cb7b7521c85c015f
Bruno Gomes <brunogomes1198@gmail.com>
2023-07-13 20:20:14 +1000

    initial files

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import os
+import psycopg2
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    # connection = psycopg2.connect(host=os.getenv("PGHOST"), user=os.getenv("PGUSER"), password=os.getenv("PGPASSWORD"), port=os.getenv("PGPORT"), dbname=os.getenv("PGDATABASE"))
+    connection = psycopg2.connect(os.getenv("DATABASE_URL", "dbname=duckspotting"))
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM mytable;")
+    results = cursor.fetchall()
+    connection.close()
+    return f"{results}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
